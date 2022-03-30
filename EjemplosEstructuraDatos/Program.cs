525513d127441f9fd41af52ae9f457208469502c
Abraham Barragan Lira <bala.barragan3@hotmail.com>
2022-03-30 16:51:17 -0700

    Modificamos y agregamos nuevos datos al codigo

--- EjemplosEstructuraDatos/Program.cs
+++ EjemplosEstructuraDatos/Program.cs
@@ -4,20 +4,28 @@ List<Alumno> lista = new List<Alumno>();
 
 Alumno alumno1 = new Alumno();
 
-alumno1.Nombres = "Antonio";
-alumno1.ApellidoPaterno = "Rogel";
-alumno1.Matricula = "123";
-alumno1.FechaNacimiento = DateTime.Now;
+alumno1.Nombres = "Isidro";
+alumno1.ApellidoPaterno = "Cedeño";
+alumno1.Matricula = "453";
+alumno1.FechaNacimiento = new DateTime(2008, 09, 13);
 
 Alumno alumno2 = new Alumno();
 
-alumno2.Nombres = "Juan";
-alumno2.ApellidoPaterno = "Lopez";
-alumno2.Matricula = "456";
-alumno2.FechaNacimiento = new DateTime(1980, 02, 28);
+alumno2.Nombres = "Aldo";
+alumno2.ApellidoPaterno = "Lujan";
+alumno2.Matricula = "789";
+alumno2.FechaNacimiento = new DateTime(2000, 10, 08);
+
+Alumno alumno3 = new Alumno();
+
+alumno3.Nombres = "Fancisco";
+alumno3.ApellidoPaterno = "Garza";
+alumno3.Matricula = "256";
+alumno3.FechaNacimiento = new DateTime(2001, 04, 21);
 
 lista.Add(alumno1);
 lista.Add(alumno2);
+lista.Add(alumno3);
 
 foreach ( Alumno alumnoEnLista in lista)
 {
